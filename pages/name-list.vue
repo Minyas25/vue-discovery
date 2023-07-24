27e439050f8029b41ed7093140892a76cbace8fb
Jean Demel <jean@m2i.fr>
2023-07-24 13:34:30 +0200

    loop and v-model

---
+++ pages/name-list.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const names = ref(['Jon', 'Riri', 'Maxou', 'Robert']);
+
+const newName = ref('');
+
+function addName(){
+    names.value.push(newName.value);
+}
+
+</script>
+
+<template>
+    <input type="text" v-model="newName" placeholder="Type a name...">
+    <button @click="addName()">Add</button>
+    <ul>
+        <li v-for="item of names">{{ item }}</li>
+    </ul>
+</template>
