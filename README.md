fcda88118e40d6f314ead981a4526ef590fa4a6b
Jean Demel <jean@m2i.fr>
2023-07-24 10:39:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vue Découverte
+Petit projet vue.js (fait avec nuxt pour avoir plus de fonctionnalités plus rapidement) pour découvrir le style de code de ce framework
+
+## How To use
+1. Cloner le projet
+2. Faire un `npm i`
+3. Lancer le projet `npm run dev`
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const first = ref('test');
+
+function changeFirst() {
+  first.value = 'autre chose';
+}
+
+</script>
+
+<template>
+  <div>
+    {{ first }}
+  </div>
+  <button @click="changeFirst()">Click !</button>
+</template>
+
+<style scoped></style>
